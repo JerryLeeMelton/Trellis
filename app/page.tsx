d2c300167b74110b6f602168712c70237dc48603
Jerry Lee Melton <jerryleemelton@gmail.com>
2026-04-03 17:35:30 -0700

    More styling changes for front page.  Rearrange jsx too.

--- app/page.tsx
+++ app/page.tsx
@@ -17,63 +17,54 @@ export default function Home() {
     <AnimPage className="main-content-container">
       {/* Intro Section ========================= */}
       <section className="intro-section-container">
-        {/* Greeting */}
-        <div className="greeting-container">
-          <div className="greeting-text-container">
+        <div className="intro-text-and-hero-image-container">
+          <div className="intro-text-container">
             <AnimH2>Hello!</AnimH2>
             <AnimH1>
               I'm <span className="text-accent-main">Jerry Lee Melton</span>.
             </AnimH1>
             <AnimH3>Full-Stack Developer based in Portland, Oregon. 🇺🇸</AnimH3>
-          </div>
-          {/* Intro text and hero image */}
-          <div className="intro-text-and-hero-image-container">
-            <div className="intro-text-container">
-              <AnimP>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Accusamus, molestias? Asperiores minima veniam delectus rerum
-                quaerat eaque placeat rem ipsum nobis, omnis aliquam, magni est
-                voluptatum, eum alias nesciunt dignissimos. Asperiores minima
-                veniam delectus rerum quaerat eaque placeat rem ipsum nobis,
-                omnis aliquam, magni est voluptatum, eum alias nesciunt
-                dignissimos.
-              </AnimP>
-              {/* Resume & Social Links */}
-              <AnimDiv className="intro-social-links-container">
-                <a href="/resume" target="_blank" rel="noopener noreferrer">
-                  <SVGIcon name="file" size={19} />
-                  Resume
-                </a>
-                <span className="opacity-40">|</span>
-                <a
-                  href="https://github.com/JerryLeeMelton"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <SVGIcon name="github" size={25} />
-                  Github
-                </a>
-                <span className="opacity-40">|</span>
-                <a
-                  href="https://www.linkedin.com/in/jerryleemelton/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <SVGIcon name="linkedin" size={22} />
-                  LinkedIn
-                </a>
-              </AnimDiv>
-            </div>
-            <AnimDiv className="greeting-image-container">
-              <Image
-                src="/images/jerry_hero_fullSize.webp"
-                alt="Picture of Jerry Lee Melton"
-                width={600}
-                height={600}
-                className="intro-image img-zoom"
-              />
+            <AnimP>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Accusamus, molestias? Asperiores minima veniam delectus rerum
+              quaerat eaque placeat rem ipsum nobis, omnis aliquam, magni est
+              voluptatum, eum alias nesciunt dignissimos.
+            </AnimP>
+            {/* Resume & Social Links */}
+            <AnimDiv className="intro-social-links-container">
+              <a href="/resume" target="_blank" rel="noopener noreferrer">
+                <SVGIcon name="file" size={19} />
+                Resume
+              </a>
+              <span className="opacity-40">|</span>
+              <a
+                href="https://github.com/JerryLeeMelton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SVGIcon name="github" size={25} />
+                Github
+              </a>
+              <span className="opacity-40">|</span>
+              <a
+                href="https://www.linkedin.com/in/jerryleemelton/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <SVGIcon name="linkedin" size={22} />
+                LinkedIn
+              </a>
             </AnimDiv>
           </div>
+          <AnimDiv className="greeting-image-container">
+            <Image
+              src="/images/jerry_hero_fullSize.webp"
+              alt="Picture of Jerry Lee Melton"
+              width={600}
+              height={600}
+              className="intro-image img-zoom"
+            />
+          </AnimDiv>
         </div>
       </section>
       <AnimDiv className="skills-section-container">
